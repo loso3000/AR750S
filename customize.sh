5769389e2ec35990161e0db2097644104ed820a5
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-09-23 19:05:53 +0800

    small-package

--- customize.sh
+++ customize.sh
@@ -19,31 +19,4 @@ sed -i 's/3.openwrt.pool.ntp.org/pool.ntp.org/g' package/base-files/files/bin/co
 # Add luci-app-ssr-plus
  git clone -b 19.07 https://github.com/kenzok8/small-package ./package/lean/small-package
 
-
-cd /workdir/lede/package/lean
-plist="shadowsocksr-libev pdnsd-alt microsocks dns2socks simple-obfs v2ray-plugin v2ray xray trojan ipt2socks redsocks2 kcptun luci-app-zerotier"
-for dir in $plist
-do
-    if [ -d $dir ]
-    then
-        echo "Copying plugin $dir to /workdir/openwrt/package/lean ..."
-        cp -rp $dir /workdir/openwrt/package/lean/
-    else
-        echo "$dir does not exists..."
-    fi
-done
-
-cd /workdir/openwrt
-[ -d /workdir/openwrt/feeds/gli_pub/shadowsocks-libev ] && mv /workdir/openwrt/feeds/gli_pub/shadowsocks-libev /workdir/openwrt/feeds/gli_pub/shadowsocks-libev.bak
-if [ -d /workdir/lede/feeds/packages/net/shadowsocks-libev ]
-then
-    [ -d /workdir/openwrt/feeds/packages/net/shadowsocks-libev ] && mv /workdir/openwrt/feeds/packages/net/shadowsocks-libev /workdir/openwrt/feeds/packages/net/shadowsocks-libev.bak
-    [ -d /workdir/openwrt/feeds/gli_pub ] && cp -r /workdir/lede/feeds/packages/net/shadowsocks-libev /workdir/openwrt/feeds/gli_pub/shadowsocks-libev
-    [ -d /workdir/openwrt/package/lean/helloworld ] && cp -r /workdir/lede/feeds/packages/net/shadowsocks-libev /workdir/openwrt/package/lean/helloworld/
-    cp -r /workdir/lede/feeds/packages/net/shadowsocks-libev /workdir/openwrt/feeds/packages/net/shadowsocks-libev
-fi
-
-rm -rf /workdir/openwrt/feeds/packages/net/wget
-cp -rp /workdir/lede/feeds/packages/net/wget /workdir/openwrt/feeds/packages/net/wget
-# Modify WiFi SSID
 #sed -i 's/set wireless.default_radio\${devidx}.ssid=OpenWrt/set wireless.default_radio\${devidx}.ssid=AR750S/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
